2904c2c43e9b9bb60e7650e2aa9155fd1072556d
Ogeto <rayogeto40@gmail.com>
2024-09-19 23:32:21 +0300

    host

--- wetha/wetha/settings.py
+++ wetha/wetha/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-wete!*yu+dl1+sv^=lqu5tf$-jpu9=6g=#6b^)flp9m-#horaf
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['wetha.onrender.com']
 
 
 # Application definition
